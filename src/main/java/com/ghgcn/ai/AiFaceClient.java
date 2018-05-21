5ba7c9e649bf6cf5e93d98260f39a05d5719cc54
yucan.zhang <zhang111307@163.com>
2018-05-21 17:14:51 +0800

    i

--- src/main/java/com/ghgcn/ai/AiFaceClient.java
+++ src/main/java/com/ghgcn/ai/AiFaceClient.java
@@ -1,6 +1,44 @@
 package com.ghgcn.ai;
 
-
 public interface AiFaceClient {
 
+    /**
+     * 注册用户
+     */
+    void addUser();
+
+    /**
+     * 注册组
+     */
+    void addGroup();
+
+    /**
+     * 人脸检测
+     */
+    void detect();
+
+    /**
+     * 人脸查找
+     */
+    void findFaces();
+
+    /**
+     * 获取用户
+     */
+    void getUser();
+
+    /**
+     * 获取用户所有人脸照片
+     */
+    void getFaces();
+
+    /**
+     * 人脸比对
+     */
+    void match();
+
+    /**
+     * 身份验证
+     */
+    void persionVerify();
 }
