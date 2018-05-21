bbcb887166a32d7ee59631003f24d9fa12f41231
yucan.zhang <zhang111307@163.com>
2018-05-21 17:16:58 +0800

    u

---
+++ src/main/java/com/ghgcn/ai/client/AliAiFaceClient.java
@@ -0,0 +1,40 @@
+package com.ghgcn.ai.client;
+
+import com.ghgcn.ai.AiFaceClient;
+
+
+public class AliAiFaceClient implements AiFaceClient {
+
+    @Override
+    public void addUser() {
+    }
+
+    @Override
+    public void addGroup() {
+    }
+
+    @Override
+    public void detect() {
+    }
+
+    @Override
+    public void findFaces() {
+    }
+
+    @Override
+    public void getUser() {
+    }
+
+    @Override
+    public void getFaces() {
+    }
+
+    @Override
+    public void match() {
+    }
+
+    @Override
+    public void persionVerify() {
+    }
+
+}
---
+++ src/main/java/com/ghgcn/ai/client/BaiduAiFaceClient.java
@@ -0,0 +1,39 @@
+package com.ghgcn.ai.client;
+
+import com.ghgcn.ai.AiFaceClient;
+
+public class BaiduAiFaceClient implements AiFaceClient {
+
+    @Override
+    public void addUser() {
+    }
+
+    @Override
+    public void addGroup() {
+    }
+
+    @Override
+    public void detect() {
+    }
+
+    @Override
+    public void findFaces() {
+    }
+
+    @Override
+    public void getUser() {
+    }
+
+    @Override
+    public void getFaces() {
+    }
+
+    @Override
+    public void match() {
+    }
+
+    @Override
+    public void persionVerify() {
+    }
+
+}
