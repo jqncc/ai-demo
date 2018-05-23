66adbc52c736e3ad5c8790d5a412eaac49192f3e
yucan.zhang <zhang111307@163.com>
2018-05-23 14:19:28 +0800

    add baidu tencent client

--- src/main/java/com/ghgcn/ai/client/BaiduAiFaceClient.java
+++ src/main/java/com/ghgcn/ai/client/BaiduAiFaceClient.java
@@ -1,11 +1,95 @@
 package com.ghgcn.ai.client;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.baidu.aip.face.AipFace;
+import com.baidu.aip.util.Base64Util;
 import com.ghgcn.ai.AiFaceClient;
 
 public class BaiduAiFaceClient implements AiFaceClient {
 
+    String APP_ID = "10533739";
+    String API_KEY = "DCkGcHtGlB8rxs24BTGbdBOQ";
+    String SECRET_KEY = "TagluzL8GT4mmEAHwEjmvWxIL6hUiKxW";
+    AipFace client = null;
+    static String groupId = "sz";
+
+    public BaiduAiFaceClient() {
+        client = new AipFace(APP_ID, API_KEY, SECRET_KEY);
+    }
+
     @Override
     public void addUser() {
+        File faceDir = new File("C:\\Users\\yucan.zhang\\Pictures\\faces");
+        String[] faceNames = faceDir.list();
+        System.out.println("face photo count:" + faceNames.length);
+        File[] faceFiles = faceDir.listFiles();
+        Map<String,String> names = new HashMap<>();
+        long s = System.nanoTime();
+        HashMap<String,String> options = new HashMap<String,String>();
+
+        options.put("quality_control", "NORMAL");// 图片质量控制 NONE: 不进行控制 LOW:较低的质量要求 NORMAL: 一般的质量要求 HIGH: 较高的质量要求 默认 NONE
+        // options.put("liveness_control", "LOW");// 活体检测控制 NONE: 不进行控制 LOW:较低的活体要求(高通过率 低攻击拒绝率) NORMAL:
+        // 一般的活体要求(平衡的攻击拒绝率,
+        // 通过率) HIGH: 较高的活体要求(高攻击拒绝率 低通过率) 默认NONE
+
+        String imageType = "BASE64";
+
+        for (File file : faceFiles) {
+            long s1 = System.nanoTime();
+            String name = file.getName().substring(0, file.getName().lastIndexOf(".")).split("_")[0];
+            String uid;
+            if (names.containsKey(name)) {
+                uid = names.get(name);
+                System.out.println("add face to :" + name + ",uid=" + uid);
+            } else {
+                uid = UUID.randomUUID().toString().replace("-", "");
+                System.out.println("add user :" + name + ",uid=" + uid);
+            }
+            String img;
+
+            try {
+                img = Base64Util.encode(Files.readAllBytes(file.toPath()));
+                options.put("user_info", file.getName());
+                JSONObject jobj = client.addUser(img, imageType, groupId, uid, options);
+                int errorCode = jobj.getInt("error_code");
+                if (errorCode == 0) {
+                    names.put(name, uid);
+                } else {
+                    // qps超限,再执行一次
+                    if (errorCode == 18) {
+                        try {
+                            Thread.sleep(400L);
+                        } catch (InterruptedException e2) {
+                            e2.printStackTrace();
+                        }
+                        jobj = client.addUser(img, imageType, groupId, uid, options);
+                        errorCode = jobj.getInt("error_code");
+                        if (errorCode == 0) {
+                            names.put(name, uid);
+                        }
+                    }
+                }
+                System.out.println(jobj);
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+            long e1 = System.nanoTime() - s1;
+            System.out.println("elapsed time:" + TimeUnit.NANOSECONDS.toMillis(e1));
+        }
+        long e = System.nanoTime() - s;
+        System.out.println("total time:" + TimeUnit.NANOSECONDS.toSeconds(e) + " , okuser:" + names.size());
+        // 每用户注册在290-1600ms,平均500ms左右,总耗时90-102s
     }
 
     @Override
@@ -30,10 +114,87 @@ public class BaiduAiFaceClient implements AiFaceClient {
 
     @Override
     public void match() {
+        String imageType = "BASE64";
+        // 2. 图片内容方式
+        long s = System.nanoTime();
+        File faceImageFile = new File("C:\\Users\\yucan.zhang\\Pictures\\compare_faces");
+        File[] files = faceImageFile.listFiles();
+        HashMap<String,String> options = new HashMap<String,String>();
+        options.put("quality_control", "NORMAL");
+        options.put("max_user_num", "3");
+        for (File file : files) {
+            long s1 = System.nanoTime();
+            String img = null;
+            try {
+                img = Base64Util.encode(Files.readAllBytes(file.toPath()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            JSONObject jobj = client.search(img, imageType, groupId, options);
+
+            int errorCode = jobj.getInt("error_code");
+            System.out.println(String.format("photo:%s compare result:", file.getName()));
+            if (errorCode == 0) {
+                System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - s1));
+                JSONObject dataNode = jobj.getJSONObject("result");
+                JSONArray userNodes = dataNode.getJSONArray("user_list");
+                Iterator<Object> it = userNodes.iterator();
+                while (it.hasNext()) {
+                    JSONObject obj = (JSONObject) it.next();
+                    String out = String.format("===>%s 置信度:%s, pid:%s,fid:%s,", obj.getString("user_info"),
+                            obj.getDouble("score"), obj.getString("user_id"), dataNode.getString("face_token"));
+                    System.out.println(out);
+                }
+            } else if (errorCode == 18) {
+                try {
+                    Thread.sleep(500L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - s1));
+                matchAndParse(imageType, options, s1, img, errorCode);
+            } else {
+                System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - s1));
+                System.out.println(jobj);
+            }
+        }
+
+        System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - s));
+        // 23张照片,人脸库151张,比对耗时:320-840ms每张,共12158ms
+    }
+
+    private void matchAndParse(String imageType, HashMap<String,String> options, long s1, String img, int errorCode) {
+        JSONObject jobj;
+        jobj = client.search(img, imageType, groupId, options);
+        if (errorCode == 0) {
+            System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - s1));
+            JSONObject dataNode = jobj.getJSONObject("result");
+            JSONArray userNodes = dataNode.getJSONArray("user_list");
+            Iterator<Object> it = userNodes.iterator();
+            while (it.hasNext()) {
+                JSONObject obj = (JSONObject) it.next();
+                String out = String.format("===>%s 置信度:%s, pid:%s,fid:%s,", obj.getString("user_info"),
+                        obj.getDouble("score"), obj.getString("user_id"), dataNode.getString("face_token"));
+                System.out.println(out);
+            }
+        }
     }
 
     @Override
     public void persionVerify() {
     }
 
+    public void deleteUsersByGroupId(String groupId) {
+        JSONObject ret = client.groupDelete(groupId, null);
+        System.out.println(ret);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BaiduAiFaceClient client = new BaiduAiFaceClient();
+        // client.addUser();
+        client.match();
+
+        // 删除用户
+        // client.deleteUsersByGroupId(groupId);
+    }
 }
--- src/main/java/com/ghgcn/ai/client/TencentAiFaceClient.java
+++ src/main/java/com/ghgcn/ai/client/TencentAiFaceClient.java
@@ -1,16 +1,119 @@
 package com.ghgcn.ai.client;
 
-import com.ghgcn.ai.AiFaceClient;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.ghgcn.ai.AiFaceClient;
+import com.qcloud.image.ImageClient;
+import com.qcloud.image.request.FaceAddFaceRequest;
+import com.qcloud.image.request.FaceAddGroupIdsRequest;
+import com.qcloud.image.request.FaceDelPersonRequest;
+import com.qcloud.image.request.FaceGetPersonIdsRequest;
+import com.qcloud.image.request.FaceIdentifyRequest;
+import com.qcloud.image.request.FaceNewPersonRequest;
 
 public class TencentAiFaceClient implements AiFaceClient {
 
+    private String bucketName = "face-1253975133";
+    private String appId = "10132068";
+    private String SecretId = "AKIDBHGQGSlVVj1z44QXF7RsXhjw0zOeMsml";
+    private String SecretKey = "WTZxBQVfw9eh0A91kRuMhy6d4jl9faVb";
+    ImageClient imageClient = null;
+
+    public TencentAiFaceClient() {
+        imageClient = new ImageClient(appId, SecretId, SecretKey);
+    }
+
     @Override
     public void addUser() {
+        // 一个 person 最多允许包含 20 个 face
+        String ret;
+        FaceNewPersonRequest personNewReq;
+        String[] groupIds = new String[2];
+        groupIds[0] = "sz";
+        groupIds[1] = "shop001";
+        String personName = "yangmi1";
+        String personId = "personId1";
+        String personTag = "star1";
+
+        /*
+        // 1. url方式
+        System.out.println("====================================================");
+        String personNewUrl = "YOUR URL";
+        personNewReq = new FaceNewPersonRequest(bucketName, personId, groupIds, personNewUrl, personName, personTag);
+        ret = imageClient.faceNewPerson(personNewReq);
+        System.out.println("person new  ret:" + ret);*/
+
+        // 2. 图片内容方式
+        /*File personNewImage = new File("E:\\codeback\\image-java-sdk-v2.0-2.2.6\\assets", "icon_porn04.jpg");
+        personNewReq = new FaceNewPersonRequest(bucketName, personId, groupIds, personNewImage, personName, personTag);
+        ret = imageClient.faceNewPerson(personNewReq);
+        // {"code":0,"message":"OK","data":{"person_id":"personId1","suc_group":2,"suc_face":1,"session_id":"","face_id":"2589490809310030953","group_ids":["shop001","sz"]}}
+        System.out.println("person new ret:" + ret);*/
+
+        /* // 3. 图片内容方式(byte[])
+        System.out.println("====================================================");
+        byte[] imageContent = getFileBytes(personNewImage);
+        if (imageContent != null) {
+            personNewReq = new FaceNewPersonRequest(bucketName, personId, groupIds, imageContent, personName,
+                    personTag);
+            ret = imageClient.faceNewPerson(personNewReq);
+            System.out.println("person new ret:" + ret);
+        } else {
+            System.out.println("person new ret: get image content fail");
+        }*/
+
+        File faceDir = new File("C:\\Users\\yucan.zhang\\Pictures\\faces");
+        String[] faceNames = faceDir.list();
+        System.out.println("face photo count:" + faceNames.length);
+        File[] faceFiles = faceDir.listFiles();
+        Map<String,String> names = new HashMap<>();
+        long s = System.nanoTime();
+        for (File file : faceFiles) {
+            long s1 = System.nanoTime();
+            String name = file.getName().substring(0, file.getName().lastIndexOf(".")).split("_")[0];
+            if (names.containsKey(name)) {
+                System.out.println("add face to :" + name);
+                String[] addPhotoNames = new String[]{ name };
+                File[] addPhotoFiles = new File[]{ file };
+                FaceAddFaceRequest addFaceReq = new FaceAddFaceRequest(bucketName, addPhotoNames, addPhotoFiles,
+                        names.get(name), file.getName());
+                ret = imageClient.faceAddFace(addFaceReq);
+                // JSONObject jobj = new JSONObject(ret);
+            } else {
+                System.out.println("add user :" + name);
+                String uid = UUID.randomUUID().toString();
+                personNewReq = new FaceNewPersonRequest(bucketName, uid, groupIds, file, name, file.getName());
+                ret = imageClient.faceNewPerson(personNewReq);
+                JSONObject jobj = new JSONObject(ret);
+                if (jobj.getInt("code") == 0) {
+                    names.put(name, uid);
+                }
+            }
+            System.out.println(ret);
+            long e1 = System.nanoTime() - s1;
+            System.out.println("elapsed time:" + TimeUnit.NANOSECONDS.toMillis(e1));
+        }
+        long e = System.nanoTime() - s;
+        System.out.println("total time:" + TimeUnit.NANOSECONDS.toSeconds(e) + " , okuser:" + names.size());
+        // 154张照片,105个用户.每用户注册在320-1650ms,总耗时130-144s
+        // 模糊照片检测不到人脸zhangdongliang
     }
 
     @Override
     public void addGroup() {
+        String ret;
+        FaceAddGroupIdsRequest request = new FaceAddGroupIdsRequest(bucketName, "personId1", "group2");
+        ret = imageClient.faceAddGroupIds(request, false);
+        System.out.println("face add group ids  ret:" + ret);
     }
 
     @Override
@@ -31,10 +134,72 @@ public class TencentAiFaceClient implements AiFaceClient {
 
     @Override
     public void match() {
+        String ret;
+
+        // 2. 图片内容方式
+        long s = System.nanoTime();
+        File faceImageFile = new File("C:\\Users\\yucan.zhang\\Pictures\\compare_faces");
+        File[] files = faceImageFile.listFiles();
+        for (File file : files) {
+            long s1 = System.nanoTime();
+            FaceIdentifyRequest faceIdentifyReq2 = new FaceIdentifyRequest(bucketName, "sz", file);// 一个 groupId
+            // FaceIdentifyRequest faceIdentifyReq2 = new FaceIdentifyRequest(bucketName, groupIds, faceImageFile);// 多个
+            // groupId
+            ret = imageClient.faceIdentify(faceIdentifyReq2);
+            System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - s1));
+            System.out.println(String.format("photo:%s compare result:", file.getName()));
+            JSONObject jobj = new JSONObject(ret);
+            if (jobj.getInt("code") == 0) {
+                JSONArray dataNodes = jobj.getJSONObject("data").getJSONArray("candidates");
+                Iterator<Object> it = dataNodes.iterator();
+                while (it.hasNext()) {
+                    JSONObject obj = (JSONObject) it.next();
+                    String out = String.format("===>%s 置信度:%s, pid:%s,fid:%s,", obj.getString("tag"),
+                            obj.getDouble("confidence"), obj.getString("person_id"), obj.getString("face_id"));
+                    System.out.println(out);
+                }
+            } else {
+                System.out.println(ret);
+            }
+        }
+
+        System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - s));
+        // 23张照片,人脸库151张,比对耗时:320-800ms每张,共4s
     }
 
     @Override
     public void persionVerify() {
     }
 
+    public void deleteUser(String pid) {
+        FaceDelPersonRequest request = new FaceDelPersonRequest(bucketName, pid);
+        String ret = imageClient.faceDelPerson(request);
+        // {"code":0,"message":"OK","data":{"deleted":1,"session_id":"","person_id":"7a8c0feb-6398-4dc0-8b13-d41470fa8552"}}
+        System.out.println(ret);
+    }
+
+    public void deleteUsersByGroupId(String groupId) {
+        FaceGetPersonIdsRequest request = new FaceGetPersonIdsRequest(bucketName, groupId);
+        String ret = imageClient.faceGetPersonIds(request);
+        JSONObject jobj = new JSONObject(ret);
+        JSONArray jarr = jobj.getJSONObject("data").getJSONArray("person_ids");
+        Iterator<Object> it = jarr.iterator();
+        while (it.hasNext()) {
+            String pid = (String) it.next();
+            FaceDelPersonRequest delrequest = new FaceDelPersonRequest(bucketName, pid);
+            String delret = imageClient.faceDelPerson(delrequest);
+            System.out.println(delret);
+        }
+
+        System.out.println(ret);
+    }
+
+    public static void main(String[] args) throws IOException {
+        TencentAiFaceClient client = new TencentAiFaceClient();
+        // client.addUser();
+        client.match();
+
+        // 删除用户
+        // client.deleteUsersByGroupId("sz");
+    }
 }
